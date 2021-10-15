ce28d4ab42fa2a587bbaa7784358dbc3b66c3d70
yoonyongmin <dydalszhffk@naver.com>
2021-10-15 13:50:49 +0900

    helperball-API

---
+++ src/test/java/com/oknym/helperball/HelperBallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oknym.helperball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperBallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
