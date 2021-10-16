528a622d0913528b6b4df696e7993e0bbcc842ee
yoonyongmin <dydalszhffk@naver.com>
2021-10-16 16:49:59 +0900

    Entity 구현

--- src/main/java/com/oknym/helperball/model/Stat.java
+++ src/main/java/com/oknym/helperball/model/Stat.java
@@ -1,5 +1,45 @@
 package com.oknym.helperball.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "helperball")
+@Getter
+@Setter
 public class Stat {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@JoinColumn
+	private Long goal;
+
+	@JoinColumn
+	private Long shoot;
+
+	@JoinColumn
+	private Long assist;
+
+	@JoinColumn
+	private Long pass;
+
+	@JoinColumn
+	private Long tackle;
+
+	@JoinColumn
+	private Long intercept;
+	
+	@ManyToOne
+	private User user;
 
 }
--- src/main/java/com/oknym/helperball/model/User.java
+++ src/main/java/com/oknym/helperball/model/User.java
@@ -1,5 +1,34 @@
 package com.oknym.helperball.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Setter;
+
+import lombok.Getter;
+
+@Entity
+@Table(name = "helperball")
+@Getter
+@Setter
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@JoinColumn
+	private String name;
+	
+	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+	private List<Stat> stat;
 
 }
