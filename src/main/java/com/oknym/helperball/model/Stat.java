e7d5b3551f39900bcb2d6eb928863a3d8d8b8137
yoonyongmin <dydalszhffk@naver.com>
2021-10-17 19:24:19 +0900

    Entity 맵핑 설정

--- src/main/java/com/oknym/helperball/model/Stat.java
+++ src/main/java/com/oknym/helperball/model/Stat.java
@@ -1,12 +1,21 @@
 package com.oknym.helperball.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,29 +25,43 @@ import lombok.Setter;
 @Setter
 public class Stat {
 	
+	public Stat() {
+		
+	}
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@JoinColumn
+	@Column(name = "goal")
 	private Long goal;
 
-	@JoinColumn
+	@Column(name = "shoot")
 	private Long shoot;
 
-	@JoinColumn
+	@Column(name = "assist")
 	private Long assist;
 
-	@JoinColumn
+	@Column(name = "pass")
 	private Long pass;
 
-	@JoinColumn
+	@Column(name = "tackle")
 	private Long tackle;
 
-	@JoinColumn
+	@Column(name = "intercept")
 	private Long intercept;
 	
-//	@ManyToOne
-//	private User user;
+	@JsonBackReference
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
+	@MapsId
+	private User user;
+	
+//	@OneToMany(mappedBy = "stat", fetch = FetchType.EAGER)
+//	private List<User> user;
+	
+//	@Column(name = "user_id")
+//	private Long userId;
+	
 
 }
--- src/main/java/com/oknym/helperball/model/User.java
+++ src/main/java/com/oknym/helperball/model/User.java
@@ -2,14 +2,21 @@ package com.oknym.helperball.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.Setter;
 
@@ -20,14 +27,26 @@ import lombok.Getter;
 @Setter
 public class User {
 	
+	public User() {
+		
+	}
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@JoinColumn
+	@Column(name = "name")
 	private String name;
 	
-//	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+	@JsonManagedReference
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "user")
+	private Stat stat;
+	
+//	@ManyToOne
+//	private Stat stat;
+	
+//	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//	@JoinColumn(name = "user_id")
 //	private List<Stat> stat;
 
 }
