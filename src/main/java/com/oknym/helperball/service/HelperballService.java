51ac932be06f246fcc5662e12cec80e0101db82e
yoonyongmin <dydalszhffk@naver.com>
2021-10-18 21:51:52 +0900

    service 추가

--- src/main/java/com/oknym/helperball/service/HelperballService.java
+++ src/main/java/com/oknym/helperball/service/HelperballService.java
@@ -2,12 +2,20 @@ package com.oknym.helperball.service;
 
 import java.util.List;
 
+import com.oknym.helperball.model.Foot;
+import com.oknym.helperball.model.Info;
+import com.oknym.helperball.model.Position;
 import com.oknym.helperball.model.Stat;
 import com.oknym.helperball.model.User;
+import com.oknym.helperball.request.InfoRequest;
 
 public interface HelperballService {
 
 	List<User> selectUser();
 	List<Stat> selectStat();
-	
+	List<Info> selectInfo();
+	List<Position> selectPosition();
+	List<Foot> selectFoot();
+//	InfoRequest saveInfo(InfoRequest infoRequest);
+
 }
