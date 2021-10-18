b42eb505443ecf3a0feb2f27253abb3115b1f198
yoonyongmin <dydalszhffk@naver.com>
2021-10-18 21:51:28 +0900

    Repository import 추가

--- src/main/java/com/oknym/helperball/service/UserRepository.java
+++ src/main/java/com/oknym/helperball/service/UserRepository.java
@@ -1,5 +1,8 @@
 package com.oknym.helperball.service;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
