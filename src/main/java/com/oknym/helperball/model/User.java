5db067b1baf82b2492a46e6cb85569ee05e0d71e
yoonyongmin <dydalszhffk@naver.com>
2021-10-19 16:54:07 +0900

    User Entity 수정

--- src/main/java/com/oknym/helperball/model/User.java
+++ src/main/java/com/oknym/helperball/model/User.java
@@ -1,21 +1,10 @@
 package com.oknym.helperball.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -29,20 +18,49 @@ public class User {
 		
 	}
 	
+//	@Builder
+//	public User(String name, String email, String picture, Role role) {
+//		this.name = name;
+//		this.email = email;
+//		this.picture = picture;
+//		this.role = role;
+//	}
+//	
+//	public User update(String name, String picture) {
+//		this.name = name;
+//		this.picture = picture;
+//		return this;
+//	}
+//	
+//	public String getRoleKey() {
+//		return this.role.getKey();
+//	}
+
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(name = "token")
+	private String token;
+	
 	@Column(name = "name")
 	private String name;
 	
-	@Column(name = "login_id")
-	private String loginId;
-	
-	@Column(name = "password")
-	private String password;
-	
 	@Column(name = "email")
 	private String email;
+	
+//	@Column(name = "email")
+//	private String email;
+//	
+//	@Column(name = "picture")
+//	private String picture;
+//	
+//	@Column(name = "role")
+//	private String role = "ROLE_USER";
+	
+//	@Enumerated(EnumType.STRING)
+//	@Column(name = "role")
+//	private Role role;
 
 }
