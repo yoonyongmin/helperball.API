56bab45cb134cfb69d538664622dace2803ab930
yoonyongmin <dydalszhffk@naver.com>
2021-10-27 21:07:34 +0900

    OauthEntity 생성

---
+++ src/main/java/com/oknym/helperball/model/Oauth.java
@@ -0,0 +1,41 @@
+package com.oknym.helperball.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Oauth {
+	
+	public Oauth() {
+		
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name = "USER_ID")
+	private String userId;
+	
+	@Column(name = "WEB")
+	private String web;
+	
+	@Column(name = "VERIFICATION")
+	private String verification;
+	
+	@Column(name = "REFRESHTOKEN")
+	private String refreshToken;
+	
+}
