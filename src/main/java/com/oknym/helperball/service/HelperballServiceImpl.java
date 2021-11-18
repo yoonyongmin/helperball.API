d85f2552c61e77eb56cb7e2cc1b3c9e2b5903730
yoonyongmin <dydalszhffk@naver.com>
2021-11-18 19:51:06 +0900

    사용하지 않는 내용 삭제

--- src/main/java/com/oknym/helperball/service/HelperballServiceImpl.java
+++ src/main/java/com/oknym/helperball/service/HelperballServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
